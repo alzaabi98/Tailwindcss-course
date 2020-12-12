0caa76962895c15b707ffe9e193c9281de55295d
Abdulaziz Alzaabi <alzaabi@Abdulazizs-MacBook-Pro-2.local>
2020-12-12 18:15:56 +0400

    adding custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      body : ['Overpass', 'sans-serif'] 
+    }
   },
   variants: {
     extend: {},
