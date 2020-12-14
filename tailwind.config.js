d87a7941e14e90936f80511b3477dc5d080305b3
Abdulaziz Alzaabi <alzaabi@Abdulazizs-MacBook-Pro-2.local>
2020-12-14 17:26:36 +0400

    adding custom colors and fix custom font issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    fontFamily: {
+    extend: {
+      fontFamily: {
       body : ['Overpass', 'sans-serif'] ,
       hero : ['Langar', 'sans-serif'] 
+    },
+    colors: {
+      primary: '#303669'
     }
+    },
+  
   },
   variants: {
     extend: {},
